8586e6efda54352c79271d5d1df9fa5504627400
Eugene Lefter <eugen.lefter@gmail.com>
2019-07-31 13:22:08 -0400

    Updates

---
+++ precache-manifest.8e0b97e3a389e091561264e9c36daa37.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d25b7f870b6ca71e78dfea7c7572b90c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e2a23e7d3dc9eafd7eb",
+    "url": "/static/css/2.066c024d.chunk.css"
+  },
+  {
+    "revision": "fcd2662b793016e78260",
+    "url": "/static/css/main.ca81ee50.chunk.css"
+  },
+  {
+    "revision": "5e2a23e7d3dc9eafd7eb",
+    "url": "/static/js/2.0d5f465b.chunk.js"
+  },
+  {
+    "revision": "fcd2662b793016e78260",
+    "url": "/static/js/main.9570e299.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
